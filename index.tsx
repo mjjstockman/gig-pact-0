ae1bd34c5467994595a523c5c89413ad61cb0fce
mjjstockman <mjjstockman@gmail.com>
2024-01-09 19:30:57 +0000

    Add basic index.tsx structure

--- index.tsx
+++ index.tsx
@@ -1,9 +1,12 @@
-import 'expo-router/entry';
-import { registerRootComponent } from 'expo';
+import React from 'react';
+import { View, Text } from 'react-native';
 
-import App from './App';
+const Index = () => {
+  return (
+    <View>
+      <Text>index</Text>
+    </View>
+  );
+};
 
-// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app in Expo Go or in a native build,
-// the environment is set up appropriately
-registerRootComponent(App);
+export default Index;
